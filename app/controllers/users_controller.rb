5f26dad013da9d2139b7f83bc031a0214863eaa4
Koma <koma@tajikfc.com>
2012-11-16 02:22:04 -0700

    Fixing Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  #before_filter :signed_in_user, only: [:show]
-  #before_filter :correct_user, only: [:show]
+  before_filter :signed_in_user, only: [:show]
+  before_filter :correct_user, only: [:show]
 
   def show
     @user = User.find(params[:id])
@@ -26,12 +26,15 @@ class UsersController < ApplicationController
 
   private
     def signed_in_user
-      redirect_to root_path, notice: "Please sign in" unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to '/signin', notice: "Please sign in"
+      end
     end
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to root_path, notice: "invalid user #{@user.id}" unless current_user?(@user)
+      redirect_back_or(root_path) unless current_user?(@user)
     end
 
 end
