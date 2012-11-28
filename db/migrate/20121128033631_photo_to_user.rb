a49fac4068ea627472e6dc172813b23b85f9cc56
Long <long@ubuntu.(none)>
2012-11-27 20:56:25 -0700

    Adding image uploads

---
+++ db/migrate/20121128033631_photo_to_user.rb
@@ -0,0 +1,7 @@
+class PhotoToUser < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128014159) do
+ActiveRecord::Schema.define(:version => 20121128035000) do
 
   create_table "posts", :force => true do |t|
     t.string   "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20121128014159) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
