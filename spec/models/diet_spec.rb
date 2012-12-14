53f06273c56221a055683a39f61d6eb75d157b5f
Alex <alex@ubuntu.(none)>
2012-12-13 19:17:24 -0700

    Added a few tests

--- spec/models/diet_spec.rb
+++ spec/models/diet_spec.rb
@@ -3,3 +3,23 @@ require 'spec_helper'
 describe Diet do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+  describe "when diet name is blank" do
+    before { @diet.name = ""}
+    it { should_not be_valid }
+  end
+
+    describe "when diet time is blank" do
+    before { @diet.time = ""}
+    it { should_not be_valid }
+  end
+
+    describe "when diet name is too big" do
+    before { @diet.name = "a" * 51}
+    it { should_not be_valid }
+  end
+
+      describe "when diet time is too big" do
+    before { @diet.time = "a" * 51}
+    it { should_not be_valid }
+  end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,3 +3,8 @@ require 'spec_helper'
 describe Post do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+      describe "when post content is blank" do
+    before { @post.content = ""}
+    it { should_not be_valid }
+  end
